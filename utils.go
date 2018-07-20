2627dcd54d12b90f907ffa97efa3aa517c7d59cb
Zack Scholl <zack.scholl@gmail.com>
2018-07-20 14:14:36 -0700

    exit if its a bad error

--- utils.go
+++ utils.go
@@ -24,6 +24,7 @@ func RunCommand(tDuration time.Duration, commands string) (string, string) {
 	err := cmd.Start()
 	if err != nil {
 		log.Error(err)
+		os.Exit(1)
 	}
 	done := make(chan error, 1)
 	go func() {
