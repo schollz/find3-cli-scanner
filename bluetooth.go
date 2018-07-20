1f2d949fa4c6434eddce5e777d66d038f444b062
Zack Scholl <zack.scholl@gmail.com>
2018-07-20 13:12:29 -0700

    revert back to btmon

--- bluetooth.go
+++ bluetooth.go
@@ -7,8 +7,8 @@ import (
 	"time"
 
 	log "github.com/cihub/seelog"
-	"github.com/go-ble/ble"
-	"github.com/go-ble/ble/examples/lib/dev"
+	"github.com/currantlabs/ble"
+	"github.com/currantlabs/ble/examples/lib/dev"
 )
 
 // sudo apt-get install bluez
@@ -17,39 +17,47 @@ var negativeNumberRegex = regexp.MustCompile(`-\d+`)
 var macAddressRegex = regexp.MustCompile(`([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})`)
 
 func scanBluetooth(out chan map[string]map[string]interface{}) {
-	var err error
+	log.Info("scanning bluetooth")
+	s := btmgmtFind()
 	data := make(map[string]map[string]interface{})
-	data["bluetooth"], err = getbluetoothScan()
-	if err != nil {
-		log.Error(err)
+	data["bluetooth"] = make(map[string]interface{})
+	for _, line := range strings.Split(s, "\n") {
+		if negativeNumberRegex.MatchString(line) && macAddressRegex.MatchString(line) {
+			rssi, err := strconv.Atoi(negativeNumberRegex.FindString(line))
+			if err != nil {
+				log.Warn(err)
+				continue
+			}
+			if rssi < minimumThreshold {
+				continue
+			}
+			data["bluetooth"][strings.ToLower(macAddressRegex.FindString(line))] = rssi
+		}
 	}
 	out <- data
 }
 
-func getbluetoothScan() (devices map[string]interface{}, err error) {
-	devices = make(map[string]interface{})
-	d, err := dev.NewDevice("default")
-	if err != nil {
-		return
-	}
-	defer d.Stop()
+func hcitoolLescan() {
+	RunCommand(4000*time.Millisecond, "hcitool lescan")
+	log.Debug("finished lescan")
+}
 
-	ble.SetDefaultDevice(d)
-	// Default to search device with name of Gopher (or specified by user).
-	filter := func(a ble.Advertisement) {
-		log.Debug(a.Addr(), a.RSSI())
-		devices[a.Addr().String()] = a.RSSI()
-	}
-
-	sd := 5 * time.Second
-	// Scan for specified durantion, or until interrupted by user.
-	log.Debugf("Scanning for %s...", sd.String())
-	ctx := ble.WithSigHandler(context.WithTimeout(context.Background(), sd))
-	errFinish := ble.Scan(ctx, false, filter, nil)
-	if errFinish != nil {
-		if !strings.Contains(errFinish.Error(), "context deadline") {
-			err = errFinish
+func btmgmtFind() string {
+	for i := 0; i < 30; i++ {
+		stdOut, stdErr := RunCommand(20*time.Second, "btmgmt find")
+		if !strings.Contains(stdErr, "Unable to start") && len(stdOut) != 0 {
+			log.Debug("finished btmgmt find")
+			return stdOut
 		}
+		RunCommand(20*time.Second, "service bluetooth restart")
+		time.Sleep(2 * time.Second)
 	}
-	return
+	return ""
+}
+
+func btmon(out chan string) {
+	s, t := RunCommand(8000*time.Millisecond, "btmon")
+	log.Debug("finished btmon")
+	out <- s
+	out <- t
 }
