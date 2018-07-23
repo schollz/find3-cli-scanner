82751d01fb3c1de01314b3c13bfe54fdd90084e3
Zack Scholl <zack.scholl@gmail.com>
2018-07-23 16:39:22 -0700

    use bluetooth library for faster processing

--- bluetooth.go
+++ bluetooth.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"regexp"
-	"strconv"
-	"strings"
 	"time"
 
 	log "github.com/cihub/seelog"
+	"github.com/go-ble/ble"
+	"github.com/go-ble/ble/examples/lib/dev"
+	"github.com/pkg/errors"
 )
 
 // sudo apt-get install bluez
@@ -16,46 +18,42 @@ var macAddressRegex = regexp.MustCompile(`([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2}
 
 func scanBluetooth(out chan map[string]map[string]interface{}) {
 	log.Info("scanning bluetooth")
-	s := btmgmtFind()
+
 	data := make(map[string]map[string]interface{})
 	data["bluetooth"] = make(map[string]interface{})
-	for _, line := range strings.Split(s, "\n") {
-		if negativeNumberRegex.MatchString(line) && macAddressRegex.MatchString(line) {
-			rssi, err := strconv.Atoi(negativeNumberRegex.FindString(line))
-			if err != nil {
-				log.Warn(err)
-				continue
-			}
-			if rssi < minimumThreshold {
-				continue
-			}
-			data["bluetooth"][strings.ToLower(macAddressRegex.FindString(line))] = rssi
-		}
+
+	d, err := dev.NewDevice("default")
+	if err != nil {
+		log.Errorf("can't new device : %s", err)
+		return
 	}
+	ble.SetDefaultDevice(d)
+
+	// Scan for specified durantion, or until interrupted by user.
+	log.Debugf("Scanning for %s...", time.Duration(scanSeconds)*time.Second)
+	ctx := ble.WithSigHandler(context.WithTimeout(context.Background(), time.Duration(scanSeconds)*time.Second))
+	devices := make(map[string][]float64)
+	advHandler := func(a ble.Advertisement) {
+		// fmt.Printf("[%s] %3d\n", a.Addr(), a.RSSI())
+		if _, ok := devices[a.Addr().String()]; !ok {
+			devices[a.Addr().String()] = []float64{}
+		}
+		devices[a.Addr().String()] = append(devices[a.Addr().String()], float64(a.RSSI()))
+	}
+	err = ble.Scan(ctx, true, advHandler, nil)
+	switch errors.Cause(err) {
+	case nil:
+	case context.DeadlineExceeded:
+		log.Debug("done")
+	case context.Canceled:
+		log.Debug("canceled\n")
+	default:
+		log.Error(err.Error())
+	}
+
+	for d := range devices {
+		data["bluetooth"][d] = int(Average(devices[d]))
+	}
+
 	out <- data
-}
-
-func hcitoolLescan() {
-	RunCommand(4000*time.Millisecond, "hcitool lescan")
-	log.Debug("finished lescan")
-}
-
-func btmgmtFind() string {
-	for i := 0; i < 30; i++ {
-		stdOut, stdErr := RunCommand(time.Duration(scanSeconds)*time.Second, "btmgmt find")
-		if !strings.Contains(stdErr, "Unable to start") && len(stdOut) != 0 {
-			log.Debug("finished btmgmt find")
-			return stdOut
-		}
-		RunCommand(20*time.Second, "service bluetooth restart")
-		time.Sleep(2 * time.Second)
-	}
-	return ""
-}
-
-func btmon(out chan string) {
-	s, t := RunCommand(8000*time.Millisecond, "btmon")
-	log.Debug("finished btmon")
-	out <- s
-	out <- t
 }
