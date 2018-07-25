28ed403d3f1f9767b17ed76d402d570dcd89e81b
Zack Scholl <zack.scholl@gmail.com>
2018-07-25 14:47:58 -0700

    bluetooth needs to be initiated only once

--- bluetooth.go
+++ bluetooth.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"strings"
+	"sync"
 	"time"
 
 	log "github.com/cihub/seelog"
@@ -9,12 +10,12 @@ import (
 )
 
 var bdata map[string]map[string]interface{}
+var bdatasync sync.Mutex
 
 func onStateChanged(d gatt.Device, s gatt.State) {
 	switch s {
 	case gatt.StatePoweredOn:
 		log.Debug("gatt powered on")
-		d.Scan([]gatt.UUID{}, false)
 		return
 	default:
 		d.StopScanning()
@@ -22,27 +23,40 @@ func onStateChanged(d gatt.Device, s gatt.State) {
 }
 
 func onPeriphDiscovered(p gatt.Peripheral, a *gatt.Advertisement, rssi int) {
+	bdatasync.Lock()
+	defer bdatasync.Unlock()
 	bdata["bluetooth"][strings.ToLower(p.ID())] = rssi
 }
 
-func scanBluetooth(out chan map[string]map[string]interface{}) {
-	log.Debug("scanning bluetooth")
+var d gatt.Device
+var bluetoothInitiated bool
 
+func scanBluetooth(out chan map[string]map[string]interface{}) (err error) {
+	if !bluetoothInitiated {
+		log.Debug("initiating bluetooth")
+		d, err = gatt.NewDevice()
+		if err != nil {
+			log.Debug("Failed to open device, err: %s", err.Error())
+			return
+		}
+		d.Handle(gatt.PeripheralDiscovered(onPeriphDiscovered))
+		d.Init(onStateChanged)
+		bluetoothInitiated = true
+	}
+
+	log.Debug("scanning bluetooth")
+	bdatasync.Lock()
 	bdata = make(map[string]map[string]interface{})
 	bdata["bluetooth"] = make(map[string]interface{})
+	bdatasync.Unlock()
 
-	d, err := gatt.NewDevice()
-	if err != nil {
-		log.Error("Failed to open device, err: %s", err)
-		return
-	}
-	// Register handlers.
-	d.Handle(gatt.PeripheralDiscovered(onPeriphDiscovered))
-	d.Init(onStateChanged)
+	d.Scan([]gatt.UUID{}, false)
 	select {
 	case <-time.After(time.Duration(scanSeconds) * time.Second):
 		log.Debug("bluetooth scan finished")
 	}
+	d.StopScanning()
 
 	out <- bdata
+	return
 }
