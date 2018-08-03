9012f58a7fa1b473dbbeb08a94f52a1f5ca869ae
Zack Scholl <zack.scholl@gmail.com>
2018-08-03 09:23:49 -0700

    make windows friendly

--- reverse_windows.go
+++ reverse_windows.go
@@ -16,8 +16,10 @@ func ReverseScan(scanTime time.Duration) (sensors models.SensorData, err error) 
 	sensors.Timestamp = time.Now().UnixNano() / int64(time.Millisecond)
 	sensors.Sensors = make(map[string]map[string]interface{})
 	err = errors.New("windows does not support gopacket")
+	log.Error(err)
+	return
 }
 
 func PromiscuousMode(on bool) {
-
+	log.Error("windows does not support promiscuous mode")
 }
