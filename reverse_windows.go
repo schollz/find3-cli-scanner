0c3f1c323b3034f6f9f892212c3764a8a577ae91
Zack Scholl <zack.scholl@gmail.com>
2018-08-03 09:22:33 -0700

    make windows friendly

---
+++ reverse_windows.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"errors"
+	"time"
+
+	log "github.com/cihub/seelog"
+	"github.com/schollz/find3/server/main/src/models"
+)
+
+func ReverseScan(scanTime time.Duration) (sensors models.SensorData, err error) {
+	log.Debugf("reverse scanning for %s", scanTime)
+	sensors = models.SensorData{}
+	sensors.Family = family
+	sensors.Device = device
+	sensors.Timestamp = time.Now().UnixNano() / int64(time.Millisecond)
+	sensors.Sensors = make(map[string]map[string]interface{})
+	err = errors.New("windows does not support gopacket")
+}
